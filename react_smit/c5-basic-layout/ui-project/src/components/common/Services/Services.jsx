937c8acff86631c9520c1ed09cf94cefcba45710
Usama1190 <usamaisrar1190@gmail.com>
2025-01-17 01:41:26 +0500

    Completed class 5 which is basic layouting and dynamic data maping on Jan 17, 25

--- react_smit/c5-basic-layout/ui-project/src/components/common/Services/Services.jsx
+++ react_smit/c5-basic-layout/ui-project/src/components/common/Services/Services.jsx
@@ -1,14 +1,25 @@
+import { ServicesCard } from '../../../utils/constant/servicesCardData';
 import Card from '../Card/Card'
 import './Service.css';
 
 const Services = () => {
+  // console.log(ServicesCard);
+  
   return (
     <div>
       <h1>Our Services</h1>
       <div className='card-wrapper'>
-      <Card />
-      <Card />
-      <Card />
+        {
+          ServicesCard.map((item, index) => {
+            return (
+              <Card data={item} key={index} />
+            )
+          })
+        }
+
+      {/* <Card data={ServicesCard[0]} />
+      <Card data={ServicesCard[1]} />
+      <Card data={ServicesCard[2]} /> */}
 
       </div>
     </div>
--- react_smit/c5-basic-layout/ui-project/src/utils/constant/servicesCardData.js
+++ react_smit/c5-basic-layout/ui-project/src/utils/constant/servicesCardData.js
@@ -2,6 +2,19 @@ export const ServicesCard = [
     {
         cardTitle: 'Card 1',
         price: 100,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis reprehenderit eligendi, quae delectus magni tempora fugit veritatis, accusantium eius adipisci nam dolores quos ea, aspernatur quaerat. Error esse maxime non?'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis reprehenderit eligendi, quae delectus magni tempora fugit veritatis, accusantium eius adipisci nam dolores quos ea, aspernatur quaerat. Error esse maxime non?',
+        isButton: true
+    },
+    {
+        cardTitle: 'Card 2',
+        price: 1000,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis reprehenderit eligendi, quae delectus magni tempora fugit veritatis, accusantium eius adipisci nam dolores quos ea, aspernatur quaerat. Error esse maxime non?',
+        isButton: false
+    },
+    {
+        cardTitle: 'Card 3',
+        price: 100000,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis reprehenderit eligendi, quae delectus magni tempora fugit veritatis, accusantium eius adipisci nam dolores quos ea, aspernatur quaerat. Error esse maxime non?',
+        isButton: true
     }
 ]
